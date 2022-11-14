fca2c196479253fffde07aed21d2fa3742e76a0a
livia araujo <liviaaraujo.dev@gmail.com>
2022-11-14 20:38:29 -0300

    screen login

--- smart-health-mobile/src/screens/Login/index.tsx
+++ smart-health-mobile/src/screens/Login/index.tsx
@@ -1,17 +1,39 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
-import { Container, Title } from "./styles";
-import logoGoogle from '../../assets/logo-google.png';
+import {
+  ContainerPage,
+  FooterContainer,
+  HeaderContainer,
+  ImgLogoGoogle,
+  InformationsContainer,
+  LogoGoogle,
+  LogoImage,
+  SocialButton,
+  SocialButtonText,
+  SubTitleText,
+  TitleText,
+} from "./styles";
+import logoGoogle from "../../assets/logo-google.png";
+import LogoApp from "../../assets/ProjectLogo.png";
 
 export function Login() {
   return (
-    <Container >
-      <Title >Smart Health</Title>
-      <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis aliquid illo fugiat. Vero eligendi expedita asperiores et veritatis a </Text>
-      <View >
-        <Text>Entrar com o google</Text>
-      <Image  source={logoGoogle}/>
-        </View>
-    </Container>
+    <ContainerPage>
+      <HeaderContainer>
+        <LogoImage style={{ width: 280, height: 280 }} source={LogoApp} />
+      </HeaderContainer>
+
+      <InformationsContainer>
+        <TitleText>YOUR HEALTH APP</TitleText>
+        <SubTitleText>SMART HEALTH</SubTitleText>
+      </InformationsContainer>
+
+      <FooterContainer>
+        <SocialButton>
+          <LogoGoogle style={{ width: 24, height: 24 }} source={logoGoogle} />
+          <SocialButtonText>Entrar com o google</SocialButtonText>
+        </SocialButton>
+      </FooterContainer>
+    </ContainerPage>
   );
 }
--- smart-health-mobile/src/screens/Login/styles.ts
+++ smart-health-mobile/src/screens/Login/styles.ts
@@ -1,17 +1,72 @@
 import styled from "styled-components/native";
 
-
-export const Container = styled.SafeAreaView`
-  flex:1;
-  justify-content: center;
-  align-items: center;
-`
-
-export const LogoContainer = styled.View`
+// ===> Config da página
+export const ContainerPage = styled.SafeAreaView`
   flex: 1;
+  background-color: #000;
 `;
 
-export const Title = styled.Text`
-  font-size: 24px;
-  font-weight: bold;
+// ===> Config da header que possui a logo
+export const HeaderContainer = styled.View`
+  flex: 2;
+  width: 100%;
+  justify: flex-start;
+  justify-content: center;
+`;
+export const LogoImage = styled.Image``;
+
+// ===> Config da container que possui os textos
+export const InformationsContainer = styled.View`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding: 10px 10%;
+`;
+export const TitleText = styled.Text`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 40px;
+  line-height: 48px;
+  width: 70%;
+  color: #f1f1f1;
+`;
+export const SubTitleText = styled.Text`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 15px;
+  color: #f1f1f1;
+`;
+
+// ===> Config da footer com botão social
+export const FooterContainer = styled.View`
+  flex: 1;
+  justify-container: center;
+  align-items: center;
+  margin-top: 10%;
+`;
+export const SocialButton = styled.TouchableOpacity`
+  display: flex;
+  flex-direction: row;
+  background: #ffffff;
+  border: 2px solid #b0b0b0;
+  border-radius: 30px;
+  padding: 4% 15%;
+`;
+export const LogoGoogle = styled.Image``;
+export const SocialButtonText = styled.Text`
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 22px;
+  display: flex;
+  align-items: center;
+  margin-left: 15px;
+  color: #2d2d2d;
+`;
+export const ImgLogoGoogle = styled.Image`
+  width: 40px;
 `;
