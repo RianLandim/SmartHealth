bd9ead120a9834e31075393c7aa79a02d69cfd2d
livia araujo <liviaaraujo.dev@gmail.com>
2022-12-12 07:04:15 -0300

    creating ranking

--- smart-health-mobile/src/screens/ranking/index.tsx
+++ smart-health-mobile/src/screens/ranking/index.tsx
@@ -14,12 +14,37 @@ export function Ranking() {
         <View style={styles.top}>
           <View style={styles.friendsRanking}>
             <Text style={styles.friendsRankingTitle}>Amigos</Text>
-            <View></View>
+            <View style={styles.friend}>
+              <Text style={styles.friendNumber}>1.</Text>
+              <Text style={styles.friendName}>Bruno</Text>
+              <View style={styles.friendBallBlueLight}></View>
+            </View>
+            <View style={styles.friend}>
+              <Text style={styles.friendNumber}>2.</Text>
+              <Text style={styles.friendName}>Diego</Text>
+              <View style={styles.friendBallOrange}></View>
+            </View>
+            <View style={styles.friend}>
+              <Text style={styles.friendNumber}>3.</Text>
+              <Text style={styles.friendName}>Maísa</Text>
+              <View style={styles.friendBallBlueLight}></View>
+            </View>
+            <View style={styles.friend}>
+              <Text style={styles.friendNumber}>4.</Text>
+              <Text style={styles.friendName}>Luana</Text>
+              <View style={styles.friendBallGreen}></View>
+            </View>
+            <View style={styles.friend}>
+              <Text style={styles.friendNumber}>5.</Text>
+              <Text style={styles.friendName}>João</Text>
+              <View style={styles.friendBallRed}></View>
+            </View>
           </View>
+
+          <TouchableOpacity style={styles.addFriendBtn}>
+            <Text style={styles.addFriendBtnText}>Adicionar Amigo</Text>
+          </TouchableOpacity>
         </View>
-        <TouchableOpacity style={styles.addFriend}>
-          <Text>Adicionar Amigo</Text>
-        </TouchableOpacity>
         <View style={styles.evolution}>
           <Text style={styles.evolutionTitle}>Evoluçao</Text>
           <Image source={evolution} />
--- smart-health-mobile/src/screens/ranking/styles.ts
+++ smart-health-mobile/src/screens/ranking/styles.ts
@@ -1,4 +1,9 @@
 import { StyleSheet } from "react-native";
+import {
+  Inter_400Regular,
+  Inter_700Bold,
+  Inter_600SemiBold,
+} from "@expo-google-fonts/inter";
 
 export const styles = StyleSheet.create({
   container: {
@@ -11,37 +16,93 @@ export const styles = StyleSheet.create({
     color: "#fff",
     fontSize: 24,
     marginTop: 40,
+    fontFamily: "Inter_400Regular",
   },
   friendsRanking: {
     backgroundColor: "#6F6F6F",
-    // width: 200,
-    height: 258,
-    width: "80%",
+    height: 240,
+    width: "45%",
+    borderRadius: 10,
+    marginBottom: 50,
   },
   friendsRankingTitle: {
     fontSize: 18,
     textAlign: "center",
+    fontFamily: "Inter_700Bold",
+    marginBottom: 20,
+    marginTop: 10,
   },
   top: {
     display: "flex",
     flexDirection: "row",
     marginTop: 20,
   },
-  addFriend: {
-    width: 140,
+  addFriendBtn: {
+    width: "35%",
     backgroundColor: "#6F6F6F",
     borderRadius: 10,
-
-    height: 50,
-    marginTop: 10,
+    height: 240,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 20,
+  },
+  addFriendBtnText: {
+    fontFamily: "Inter_700Bold",
+    fontSize: 24,
+    textAlign: "center",
   },
   evolutionTitle: {
     fontSize: 24,
     textAlign: "center",
+    fontFamily: "Inter_700Bold",
   },
   evolution: {
     backgroundColor: "#6F6F6F",
     width: "90%",
     height: 340,
+  },
+  friend: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  friendNumber: {
+    fontSize: 14,
+  },
+  friendName: {
+    fontSize: 21,
+    fontFamily: "Inter_400Regular",
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  friendBallBlueLight: {
+    backgroundColor: "#38B6FF",
+    borderRadius: 80,
+    width: 16,
+    height: 16,
+  },
+  friendBallOrange: {
+    backgroundColor: "#FF914D",
+    borderRadius: 80,
+    width: 16,
+    height: 16,
+  },
+  friendBallBLue: {
+    backgroundColor: "#5E17EB",
+    borderRadius: 80,
+    width: 16,
+    height: 16,
+  },
+  friendBallGreen: {
+    backgroundColor: "#7ED957",
+    borderRadius: 80,
+    width: 16,
+    height: 16,
+  },
+  friendBallRed: {
+    backgroundColor: "#FF5757",
+    borderRadius: 80,
+    width: 16,
+    height: 16,
   },
 });
